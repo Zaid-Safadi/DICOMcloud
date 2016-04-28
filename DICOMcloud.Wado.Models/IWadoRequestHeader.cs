45a3365836f83b56dac076bccce2af9fb484ab71
zaid al-safadi <zsafadi11@gmail.com>
2016-04-27 23:19:06 -0400

    Add WADO models project

---
+++ DICOMcloud.Wado.Models/IWadoRequestHeader.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DICOMcloud.Wado.Models
+{
+   public interface IWadoRequestHeader
+   {
+      HttpHeaderValueCollection<MediaTypeWithQualityHeaderValue> AcceptHeader { get; set; }
+      HttpHeaderValueCollection<StringWithQualityHeaderValue> AcceptCharsetHeader { get; set; }
+   
+   }
+}
