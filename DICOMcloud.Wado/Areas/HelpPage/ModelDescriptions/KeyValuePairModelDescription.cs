d002df8a62191abae27effdafeb220184f035eb4
zaid al-safadi <zsafadi11@gmail.com>
2016-04-27 23:13:28 -0400

    code cleanup

--- DICOMcloud.Wado/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
+++
@@ -1,9 +0,0 @@
-namespace DICOMcloud.Wado.Areas.HelpPage.ModelDescriptions
-{
-    public class KeyValuePairModelDescription : ModelDescription
-    {
-        public ModelDescription KeyModelDescription { get; set; }
-
-        public ModelDescription ValueModelDescription { get; set; }
-    }
-}
--- DICOMcloud.Wado/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DICOMcloud.Wado.WebApiApplication" Language="C#" %>
